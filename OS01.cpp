c01f7d3fd713cb384e35e29f4867c1a66daeabf3
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-09-11 10:43:47 +0900

    Add files via upload

---
+++ OS01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int start = 1; start <= n; start += 5) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = start; j < start + 5 && j <= n; j++) {
+				if (j < i)
+					cout << '\t' << '\t';
+				else
+					cout << j << " x " << i << " = " << i * j << '\t';
+			}
+			cout << '\n';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
