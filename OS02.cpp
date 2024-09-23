9ad3e7e67acfcc2c2a5fac35d93a4ac0f4721c32
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-09-23 19:16:49 +0900

    Update OS02.cpp

--- OS02.cpp
+++ OS02.cpp
@@ -1,27 +1,38 @@
-//EnumProcesses();
-//GetLocalTime();
 #include <iostream>
 #include <windows.h>
+#include <psapi.h>
+#define ONE_SEC 1000
 using namespace std;
 
-void CountProcess() {
+// 컴퓨터의 현재 프로세스 수 계산
+DWORD CountProcess() {
+    DWORD processIDs[1024], cbNeeded;
+    EnumProcesses(processIDs, sizeof(processIDs), &cbNeeded);
+    DWORD processCount = cbNeeded / sizeof(DWORD);
 
+    return processCount;  
 }
 
+// 1초마다 화면에 현재시간, 프로세스 수 출력
 void Print_per_sec() {
-
-}
-int main() {
     SYSTEMTIME st;
-    GetLocalTime(&st);
 
+    while (true) {
+        GetLocalTime(&st);
+        DWORD processCount = CountProcess();
+        cout << st.wYear << "."
+            << st.wMonth << "."
+            << st.wDay << " "
+            << st.wHour << ":"
+            << st.wMinute << ":"
+            << st.wSecond << " : "
+            << processCount << '\n';
+        Sleep(ONE_SEC);
+    }
+}
 
-    cout << st.wYear << "."
-        << st.wMonth << "."
-        << st.wDay << " "
-        << st.wHour << ":"
-        << st.wMinute << ":"
-        << st.wSecond << " : " << '\n';
-   
+int main() {
+    Print_per_sec();
+
     return 0;
 }
