42c293485a5a1e68cd7b7bcbe728f0a8a3b860f0
HyungjuunPark <141126343+HyungjuunPark@users.noreply.github.com>
2024-10-21 02:07:35 +0900

    Update OS01.cpp

--- OS01.cpp
+++ OS01.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-
 	for (int start = 1; start <= n; start += 5) {
 		for (int i = 1; i <= n; i++) {
 			for (int j = start; j < start + 5 && j <= n; j++) {
